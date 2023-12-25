2a2db6bfcad8623dbd1385d0d1ef561dfd1430b8
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2023-12-25 21:41:09 +0800

    added coloring rainbow functionality. i learned a new thing today. you can use 'this' inside functions

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const drawingboard = document.querySelector(".drawingboard");
+
+function random(number) {
+  return Math.floor(Math.random() * number + 1);
+}
+
 function buildGrid(gridSize) {
   
   let newRow;
   let box;
+  let isMousePressed = false
+
+  //handling mouse events for drawing
+  function handleMouseDown() {
+    isMousePressed = true;
+  }
+  
+  function handleMouseUp() {
+    isMousePressed = false;
+  }
+  
+  function handleMouseHover() {
+    if (isMousePressed) {
+      this.style["background-color"] = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    }
+  }
+
+  //start grid build
   for (let i = 0; i < gridSize; i++) {
     newRow = document.createElement("div");
     newRow.style.cssText = "display: flex; flex: 1;";
@@ -10,20 +33,17 @@ function buildGrid(gridSize) {
     for (let j = 0; j < gridSize; j++) {
       box = document.createElement("div");
       box.style.cssText = "flex:1; border: 0.5px solid hsl(0, 0%, 60%);";
-      function addClickEvent(currentBox) {
-        currentBox.addEventListener("click", () => {
-            currentBox.style["background"] = "black";
-        });
-    }
-
-    // Call the function with the current box
-    addClickEvent(box);
+      box.addEventListener("mousedown", handleMouseDown);
+      box.addEventListener("mouseup", handleMouseUp);
+      box.addEventListener("mouseover", handleMouseHover);
       newRow.append(box);
     }
-
+    
     drawingboard.append(newRow);
   }
 }
+
+
 
 function removeGrid(){
     drawingboard.innerHTML = '';
